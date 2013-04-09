a6bc56adc2480cddc5e3be324ffb03b36df4bc14
Carl-Emil Kjellstrand <erbsman@gmail.com>
2013-04-09 08:09:36 +0100

    Initial commit, nothing realy works, just a bunch of text feilds in the widget for now.

---
+++ src/se/kjellstrand/colorclock/ColorClockAppWidgetProvider.java
@@ -0,0 +1,34 @@
+package se.kjellstrand.colorclock;
+import se.kjellstrand.colorclock.R;
+import se.kjellstrand.colorclock.activity.ColorClock;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+
+public class ColorClockAppWidgetProvider extends AppWidgetProvider {
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, ColorClock.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.color_clock);
+            //views.setOnClickPendingIntent(R.id.button, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
