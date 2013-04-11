aeefe1038df6a04ca4d70061eb8cbb1eee3dd2fe
erbsman <erbsman@gmail.com>
2013-04-11 22:58:04 +0100

    cleanup and docs.

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ ColorClock
 ==========
 
 A clock widget for Android
+
+ideeas:
+
+* use gradients as backgrounds allowing for 3 diffrent colors to blen in one digit, h:m:s style.
+
+* add "mode" to settings, normal(with digital clock on top.), hard(remove digital clock), expert(remove text digits, only color) ?
+
+* settings for the 3 "base" colors  (h:m:s).
+
+* multiple layours to select from.
+
+
--- src/se/kjellstrand/colorclock/provider/ColorClockAppWidgetProvider.java
+++ src/se/kjellstrand/colorclock/provider/ColorClockAppWidgetProvider.java
@@ -18,37 +18,135 @@ public class ColorClockAppWidgetProvider extends AppWidgetProvider {
     private static final String TAG = ColorClockAppWidgetProvider.class
             .getName();
 
-    int[] mAppWidgetIds;
-    AppWidgetManager widgetManager;
-    static Timer timer = null;
-
-    private final int[] mDigitsColor = new int[10];
-
+    /**
+     * List of the ids on the digit views.
+     */
     private final static int[] DIGIT_VIEWS_INDEX = new int[] {
             R.id.digit_0, R.id.digit_1, R.id.digit_2, R.id.digit_3,
             R.id.digit_4, R.id.digit_5, R.id.digit_6, R.id.digit_7,
             R.id.digit_8, R.id.digit_9
     };
 
-    private RemoteViews views;
+    /**
+     * Updates AppWidget state; gets information about installed AppWidget
+     * providers and other AppWidget related state.
+     */
+    private AppWidgetManager mWidgetManager;
 
-    private int mSSColor = 0xff0000cc;
-    private int mSColor = 0xff000099;
+    /**
+     * The appWidgetIds for which an update is needed. Note that this may be all
+     * of the AppWidget instances for this provider, or just a subset of them.
+     */
+    private int[] mAppWidgetIds;
 
-    private int mMMColor = 0xff00cc00;
-    private int mMColor = 0xff009900;
+    /**
+     * Timer that fires once per second, and updates the colors of the clock.
+     */
+    private static Timer timer = null;
 
-    private int mHHColor = 0xffcc0000;
-    private int mHColor = 0xff990000;
+    /**
+     * Holds the current colors of each digit, used while calculating the color
+     * state of the clock in each update.
+     */
+    private final int[] mDigitsColor = new int[10];
 
+    /**
+     * Holds the views of the current widget layout.
+     */
+    private RemoteViews mRemoteViews;
+
+    private double mSecondaryColorStrength = 0.7d;
+
+    /**
+     * Max value for the alpha channel in 32 bit argb. Used for bit
+     * manipulations of the colors.
+     */
+    int mAlphaChannelMaxed = 0xff000000;
+
+    /**
+     * Major color for hours, displayed on the first digit of the hours. So if
+     * the clock is 12:34:56 then the 1 would get this color as background
+     * color.
+     */
+    private int mHHColor = 0xffd03020;
+    
+    /**
+     * Minor color for hours, displayed on the second digit of the hours. So if
+     * the clock is 12:34:56 then the 2 would get this color as background
+     * color.
+     */
+    private int mHColor = mHHColor;
+    
+    /**
+     * Major color for minutes, displayed on the first digit of the minutes. So
+     * if the clock is 12:34:56 then the 3 would get this color as background
+     * color.
+     */
+    private int mMMColor = 0xff30d020;
+    
+    /**
+     * Minor color for minutes, displayed on the second digit of the minutes. So
+     * if the clock is 12:34:56 then the 4 would get this color as background
+     * color.
+     */
+    private int mMColor = mMMColor;
+    
+    /**
+     * Major color for seconds, displayed on the first digit of the seconds. So
+     * if the clock is 12:34:56 then the 5 would get this color as background
+     * color.
+     */
+    private int mSSColor = 0xff3020d0;
+    
+    /**
+     * Minor color for seconds, displayed on the second digit of the seconds. So
+     * if the clock is 12:34:56 then the 6 would get this color as background
+     * color.
+     */
+    private int mSColor = mSSColor;
+
+    /**
+     * What color will digits without a specific background set get, starts uninitialised. 
+     */
     private int mDefaultDigitBackgrundColor = -1;
 
+    /**
+     * Constructor, updates the secondary colors.
+     */
     public ColorClockAppWidgetProvider() {
-        Log.d(TAG, "ColorClockAppWidgetProvider");
+        upateSecondaryColors();
     }
 
+    /**
+     * 
+     */
+    private void upateSecondaryColors() {
+        mSColor = (mAlphaChannelMaxed
+                + ((int) ((mSSColor & 0xff0000) * mSecondaryColorStrength) & 0xff0000)
+                + ((int) ((mSSColor & 0xff00) * mSecondaryColorStrength) & 0xff00) + ((int) ((mSSColor & 0xff) * mSecondaryColorStrength) & 0xff0));
+
+        mMColor = (int) (mAlphaChannelMaxed
+                + ((int) ((mMMColor & 0xff0000) * mSecondaryColorStrength) & 0xff0000)
+                + ((int) ((mMMColor & 0xff00) * mSecondaryColorStrength) & 0xff00) + ((int) ((mMMColor & 0xff) * mSecondaryColorStrength) & 0xff0));
+
+        mHColor = (int) (mAlphaChannelMaxed
+                + ((int) ((mHHColor & 0xff0000) * mSecondaryColorStrength) & 0xff0000)
+                + ((int) ((mHHColor & 0xff00) * mSecondaryColorStrength) & 0xff00) + ((int) ((mHHColor & 0xff) * mSecondaryColorStrength) & 0xff0));
+
+        Log.d(TAG, "mSColor " + Integer.toHexString(mSColor));
+        Log.d(TAG, "mMColor " + Integer.toHexString(mMColor));
+        Log.d(TAG, "mHColor " + Integer.toHexString(mHColor));
+
+    }
+
+    /**
+     * 
+     * 
+     * @param context
+     */
     public void update(Context context) {
-        views = new RemoteViews(context.getPackageName(), R.layout.color_clock);
+        mRemoteViews = new RemoteViews(context.getPackageName(),
+                R.layout.color_clock);
 
         if (mDefaultDigitBackgrundColor == -1) {
             mDefaultDigitBackgrundColor = context.getResources().getColor(
@@ -83,31 +181,24 @@ public class ColorClockAppWidgetProvider extends AppWidgetProvider {
             }
         }
 
-        // views.setTextViewText(R.id.digit_0, mm + "" + m + ":" + ss + "" + s);
-
         // Set the colors to the views.
         for (int i = 0; i <= 9; i++) {
-            views.setInt(DIGIT_VIEWS_INDEX[i], "setBackgroundColor",
+            mRemoteViews.setInt(DIGIT_VIEWS_INDEX[i], "setBackgroundColor",
                     mDigitsColor[i]);
-            // Log.d(TAG, "mDigitsColor["+i+"]"+mDigitsColor[i]);
         }
 
-        // Log.d(TAG, "Time: " + (System.currentTimeMillis() / 10000) % 6 + ":"
-        // + (System.currentTimeMillis() / 1000) % 10);
-
-        widgetManager.updateAppWidget(mAppWidgetIds, views);
-
+        mWidgetManager.updateAppWidget(mAppWidgetIds, mRemoteViews);
     }
 
     private int additiveBlendTwoColors(int c1, int c2) {
         int r = Math.min(((c1 & 0xff0000) + (c2 & 0xff0000)), 0xff0000);
         int g = Math.min(((c1 & 0xff00) + (c2 & 0xff00)), 0xff00);
         int b = Math.min(((c1 & 0xff) + (c2 & 0xff)), 0xff);
-        int alpha = 0xff000000;
-        int c = alpha + r + g + b;
+        int c = mAlphaChannelMaxed + r + g + b;
         return c;
     }
 
+    @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         CharSequence text = "SHUTDOWN ";
         int duration = Toast.LENGTH_LONG;
@@ -116,13 +207,14 @@ public class ColorClockAppWidgetProvider extends AppWidgetProvider {
         timer.cancel();
     }
 
+    @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
             int[] appWidgetIds) {
         // lets copy our stuff
         mAppWidgetIds = new int[appWidgetIds.length];
         for (int a = 0; a < appWidgetIds.length; a++)
             mAppWidgetIds[a] = appWidgetIds[a];
-        widgetManager = appWidgetManager;
+        mWidgetManager = appWidgetManager;
 
         update(context);
 
@@ -144,7 +236,6 @@ public class ColorClockAppWidgetProvider extends AppWidgetProvider {
     }
 
     private class MyTime extends TimerTask {
-        AppWidgetManager appWidgetManager;
         ColorClockAppWidgetProvider parent;
         Context context;
 
