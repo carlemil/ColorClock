4c15925f3e679524baa7b3bead677fb46d052e37
erbsman <erbsman@gmail.com>
2013-04-24 13:09:53 +0100

    rewrite to using a service and alarm to update the widgets.

--- src/se/kjellstrand/colorclock/provider/ColorClockAppWidgetProvider.java
+++ src/se/kjellstrand/colorclock/provider/ColorClockAppWidgetProvider.java
@@ -1,18 +1,16 @@
 package se.kjellstrand.colorclock.provider;
 
 import java.util.Calendar;
-import java.util.Timer;
-import java.util.TimerTask;
 
-import se.kjellstrand.colorclock.R;
-import se.kjellstrand.colorclock.util.ColorUtil;
-
+import se.kjellstrand.colorclock.service.ClockService;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
-import android.widget.RemoteViews;
-import android.widget.Toast;
 
 public class ColorClockAppWidgetProvider extends AppWidgetProvider {
 
@@ -22,252 +20,53 @@ public class ColorClockAppWidgetProvider extends AppWidgetProvider {
     private static final String TAG = ColorClockAppWidgetProvider.class
             .getName();
 
-    /**
-     * List of the ids on the digit views.
-     */
-    private final static int[] DIGIT_VIEWS_INDEX = new int[] {
-            R.id.digit_0, R.id.digit_1, R.id.digit_2, R.id.digit_3,
-            R.id.digit_4, R.id.digit_5, R.id.digit_6, R.id.digit_7,
-            R.id.digit_8, R.id.digit_9
-    };
-    
-    /**
-     * Holds the current colors of each digit, used while calculating the color
-     * state of the clock in each update.
-     */
-    private final int[] mDigitsColor = new int[10];
+    private static final Intent mUpdateIntent = new Intent(
+            ClockService.ACTION_UPDATE);
 
-    /**
-     * Updates AppWidget state; gets information about installed AppWidget
-     * providers and other AppWidget related state.
-     */
-    private AppWidgetManager mWidgetManager;
+    private static final int REQUEST_CODE = 810528;
 
-    /**
-     * The appWidgetIds for which an update is needed. Note that this may be all
-     * of the AppWidget instances for this provider, or just a subset of them.
-     */
-    private int[] mAppWidgetIds;
-
-    /**
-     * Timer that fires once per second, and updates the colors of the clock.
-     */
-    private static Timer mTimer = null;
-
-    /**
-     * Holds the views of the current widget layout.
-     */
-    private RemoteViews mRemoteViews;
-
-    /**
-     * Determines how strong the secondary color is, the color showing (shown in
-     * caps) hH:mM:sS.
-     */
-    private double mSecondaryColorStrength = 0.7d;
-
-    /**
-     * Major color for hours, displayed on the first digit of the hours. So if
-     * the clock is 12:34:56 then the 1 would get this color as background
-     * color.
-     */
-    private int mPrimaryHourColor = 0xd0f03020;
-
-    /**
-     * Minor color for hours, displayed on the second digit of the hours. So if
-     * the clock is 12:34:56 then the 2 would get this color as background
-     * color.
-     */
-    private int mSecondaryHourColor = mPrimaryHourColor;
-
-    /**
-     * Major color for minutes, displayed on the first digit of the minutes. So
-     * if the clock is 12:34:56 then the 3 would get this color as background
-     * color.
-     */
-    private int mPrimaryMinuteColor = 0xd030f020;
-
-    /**
-     * Minor color for minutes, displayed on the second digit of the minutes. So
-     * if the clock is 12:34:56 then the 4 would get this color as background
-     * color.
-     */
-    private int mSecondaryMinuteColor = mPrimaryMinuteColor;
-
-    /**
-     * Major color for seconds, displayed on the first digit of the seconds. So
-     * if the clock is 12:34:56 then the 5 would get this color as background
-     * color.
-     */
-    private int mPrimarySecondColor = 0xd03020f0;
-
-    /**
-     * Minor color for seconds, displayed on the second digit of the seconds. So
-     * if the clock is 12:34:56 then the 6 would get this color as background
-     * color.
-     */
-    private int mSecondarySecondColor = mPrimarySecondColor;
-
-    /**
-     * What color will digits without a specific background set get, starts
-     * uninitialised.
-     */
-    private int mDefaultDigitBackgrundColor = -1;
-
-    /**
-     * Constructor, updates the secondary colors.
-     */
-    public ColorClockAppWidgetProvider() {
-        upateSecondaryColors();
-    }
-
-    /**
-     * Updates the secondary colors to be fractions of the primary colors.
-     */
-    private void upateSecondaryColors() {
-        mSecondaryHourColor = ColorUtil.getSecondaryColorFromPrimaryColor(
-                mPrimaryHourColor, mSecondaryColorStrength);
-        mSecondaryMinuteColor = ColorUtil.getSecondaryColorFromPrimaryColor(
-                mPrimaryMinuteColor, mSecondaryColorStrength);
-        mSecondarySecondColor = ColorUtil.getSecondaryColorFromPrimaryColor(
-                mPrimarySecondColor, mSecondaryColorStrength);
-    }
-
-    /**
-     * Updates the colors of the clock to a state representing "now".
-     * 
-     * @param context used to grab hold of some xml resource values such as
-     *        default background color.
-     */
-    public void update(Context context) {
-        mRemoteViews = new RemoteViews(context.getPackageName(),
-                R.layout.color_clock);
-
-        if (mDefaultDigitBackgrundColor == -1) {
-            mDefaultDigitBackgrundColor = context.getResources().getColor(
-                    R.color.default_digit_background_color);
-        }
-
-        Calendar c = Calendar.getInstance();
-
-        int hoursX0 = c.get(Calendar.HOUR_OF_DAY) / 10;
-        int hours0X = c.get(Calendar.HOUR_OF_DAY) % 10;
-        int minutesX0 = c.get(Calendar.MINUTE) / 10;
-        int minutes0X = c.get(Calendar.MINUTE) % 10;
-        int secondsX0 = c.get(Calendar.SECOND) / 10;
-        int seconds0X = c.get(Calendar.SECOND) % 10;
-
-        for (int i = 0; i <= 9; i++) {
-            mDigitsColor[i] = 0;
-        }
-
-        mDigitsColor[hoursX0] = setOrBlendDigitColorWithColor(
-                mDigitsColor[hoursX0], mPrimaryHourColor);
-        mDigitsColor[hours0X] = setOrBlendDigitColorWithColor(
-                mDigitsColor[hours0X], mSecondaryHourColor);
-        mDigitsColor[minutesX0] = setOrBlendDigitColorWithColor(
-                mDigitsColor[minutesX0], mPrimaryMinuteColor);
-        mDigitsColor[minutes0X] = setOrBlendDigitColorWithColor(
-                mDigitsColor[minutes0X], mSecondaryMinuteColor);
-        mDigitsColor[secondsX0] = setOrBlendDigitColorWithColor(
-                mDigitsColor[secondsX0], mPrimarySecondColor);
-        mDigitsColor[seconds0X] = setOrBlendDigitColorWithColor(
-                mDigitsColor[seconds0X], mSecondarySecondColor);
-
-        for (int i = 0; i <= 9; i++) {
-            if (mDigitsColor[i] == 0) {
-                mDigitsColor[i] = mDefaultDigitBackgrundColor;
-            }
-        }
-
-        // Set the colors to the views.
-        for (int i = 0; i <= 9; i++) {
-            //
-            mRemoteViews.setInt(DIGIT_VIEWS_INDEX[i], "setBackgroundColor",
-                    mDigitsColor[i]);
-        }
-
-        mWidgetManager.updateAppWidget(mAppWidgetIds, mRemoteViews);
-    }
-
-    /**
-     * Blends the two colors unless the first color is pitch black with 0 alpha,
-     * for that corner-case it will return the second color
-     * 
-     * @param c1 first color.
-     * @param c2 second color.
-     * @return a blend of the two color, unless above stated condition applies.
-     */
-    private int setOrBlendDigitColorWithColor(int c1, int c2) {
-        if (c1 != 0) {
-            return ColorUtil.screenBlendTwoColors(c1, c2);
-        } else {
-            return c2;
-        }
-    }
-
-    @Override
-    public void onDeleted(Context context, int[] appWidgetIds) {
-        int duration = Toast.LENGTH_LONG;
-        Toast toast = Toast.makeText(context, "Shuting down Clock widget.",
-                duration);
-        toast.show();
-        if (mTimer != null) {
-            mTimer.cancel();
-        }
-    }
+    private Context mContext = null;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
             int[] appWidgetIds) {
-        // lets copy our stuff
-        mAppWidgetIds = new int[appWidgetIds.length];
-        for (int a = 0; a < appWidgetIds.length; a++)
-            mAppWidgetIds[a] = appWidgetIds[a];
-        mWidgetManager = appWidgetManager;
-
-        update(context);
-
-        if (mTimer == null) {
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(context, "Instaling Clock widget.",
-                    duration);
-            toast.show();
-
-            mTimer = new Timer();
-            Calendar cal = Calendar.getInstance();
-            cal.add(Calendar.SECOND, 1);
-            cal.set(Calendar.MILLISECOND, 500);
-
-            mTimer.scheduleAtFixedRate(new MyTime(context, this), cal.getTime(),
-                    1000);
-        }
-
+        this.mContext = context;
+        this.mContext.startService(mUpdateIntent);
+        scheduleTimer();
     }
 
-    /**
-     * Timer that sends updates once a second to the clock digits.
-     * 
-     */
-    private class MyTime extends TimerTask {
-        private ColorClockAppWidgetProvider parent;
-        private Context context;
-
-        public MyTime(Context context, ColorClockAppWidgetProvider parent) {
-            this.parent = parent;
-            this.context = context;
-        }
-
-        @Override
-        public void run() {
-            try {
-                parent.update(context);
-            } catch (Exception e) {
-                CharSequence text = "tim_excp : " + e.getMessage();
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        Log.d(TAG, "onDeleted");
+        AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+        int[] remainingIds = mgr.getAppWidgetIds(new ComponentName(context,
+                this.getClass()));
+        if (remainingIds == null || remainingIds.length <= 0) {
+            PendingIntent pi = PendingIntent.getService(context, REQUEST_CODE,
+                    mUpdateIntent, PendingIntent.FLAG_NO_CREATE);
+            if (pi != null) {
+                AlarmManager am = (AlarmManager) context
+                        .getSystemService(Context.ALARM_SERVICE);
+                am.cancel(pi);
+                pi.cancel();
+                Log.d(TAG, "Alarm cancelled");
             }
         }
     }
 
+    private void scheduleTimer() {
+        Calendar date = Calendar.getInstance();
+        date.set(Calendar.SECOND, 1);
+        date.set(Calendar.MILLISECOND, 500);
+        AlarmManager am = (AlarmManager) mContext
+                .getSystemService(Context.ALARM_SERVICE);
+        PendingIntent pi = PendingIntent.getService(mContext, REQUEST_CODE,
+                mUpdateIntent, PendingIntent.FLAG_NO_CREATE);
+        if (pi == null) {
+            pi = PendingIntent.getService(mContext, REQUEST_CODE,
+                    mUpdateIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+            am.setRepeating(AlarmManager.RTC, date.getTimeInMillis(), 1000, pi);
+            Log.d(TAG, "Alarm created");
+        }
+    }
 }
---
+++ src/se/kjellstrand/colorclock/service/ClockService.java
@@ -0,0 +1,194 @@
+package se.kjellstrand.colorclock.service;
+
+import java.util.Calendar;
+import se.kjellstrand.colorclock.R;
+import se.kjellstrand.colorclock.provider.ColorClockAppWidgetProvider;
+import se.kjellstrand.colorclock.util.ColorUtil;
+import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+public class ClockService extends IntentService {
+
+    private static final String TAG = ClockService.class.getCanonicalName();
+
+    public static final String ACTION_UPDATE = "se.kjellstrand.colorclock.ACTION_UPDATE";
+
+    /**
+     * List of the ids on the digit views.
+     */
+    private final static int[] DIGIT_VIEWS_INDEX = new int[] {
+            R.id.digit_0, R.id.digit_1, R.id.digit_2, R.id.digit_3,
+            R.id.digit_4, R.id.digit_5, R.id.digit_6, R.id.digit_7,
+            R.id.digit_8, R.id.digit_9
+    };
+
+    /**
+     * Holds the current colors of each digit, used while calculating the color
+     * state of the clock in each update.
+     */
+    private final int[] mDigitsColor = new int[10];
+
+    /**
+     * Determines how strong the secondary color is, the color showing (shown in
+     * caps) hH:mM:sS.
+     */
+    private double mSecondaryColorStrength = 0.7d;
+
+    /**
+     * Major color for hours, displayed on the first digit of the hours. So if
+     * the clock is 12:34:56 then the 1 would get this color as background
+     * color.
+     */
+    private int mPrimaryHourColor = 0xffff0000;
+
+    /**
+     * Minor color for hours, displayed on the second digit of the hours. So if
+     * the clock is 12:34:56 then the 2 would get this color as background
+     * color.
+     */
+    private int mSecondaryHourColor = ColorUtil
+            .getSecondaryColorFromPrimaryColor(mPrimaryHourColor,
+                    mSecondaryColorStrength);
+
+    /**
+     * Major color for minutes, displayed on the first digit of the minutes. So
+     * if the clock is 12:34:56 then the 3 would get this color as background
+     * color.
+     */
+    private int mPrimaryMinuteColor = 0xff00ff00;
+
+    /**
+     * Minor color for minutes, displayed on the second digit of the minutes. So
+     * if the clock is 12:34:56 then the 4 would get this color as background
+     * color.
+     */
+    private int mSecondaryMinuteColor = ColorUtil
+            .getSecondaryColorFromPrimaryColor(mPrimaryMinuteColor,
+                    mSecondaryColorStrength);
+
+    /**
+     * Major color for seconds, displayed on the first digit of the seconds. So
+     * if the clock is 12:34:56 then the 5 would get this color as background
+     * color.
+     */
+    private int mPrimarySecondColor = 0xff0000ff;
+
+    /**
+     * Minor color for seconds, displayed on the second digit of the seconds. So
+     * if the clock is 12:34:56 then the 6 would get this color as background
+     * color.
+     */
+    private int mSecondarySecondColor = ColorUtil
+            .getSecondaryColorFromPrimaryColor(mPrimarySecondColor,
+                    mSecondaryColorStrength);
+
+    /**
+     * What color will digits without a specific background set get, starts
+     * uninitialised.
+     */
+    private int mDefaultDigitBackgrundColor;
+
+    public ClockService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (mDefaultDigitBackgrundColor == 0) {
+            mDefaultDigitBackgrundColor = getResources().getColor(
+                    R.color.default_digit_background_color);
+        }
+        
+        if (intent.getAction().equals(ACTION_UPDATE)) {
+            Calendar now = Calendar.getInstance();
+            updateAllViews(now);
+        }
+    }
+
+    private void updateAllViews(Calendar calendar) {
+        Log.d(TAG, "Update: " + calendar.getTime());
+        AppWidgetManager manager = AppWidgetManager.getInstance(this);
+        ComponentName name = new ComponentName(this,
+                ColorClockAppWidgetProvider.class);
+        int[] appIds = manager.getAppWidgetIds(name);
+        for (int id : appIds) {
+            RemoteViews remoteViews = new RemoteViews(getPackageName(),
+                    R.layout.color_clock);
+            updateView(remoteViews, calendar);
+            manager.updateAppWidget(id, remoteViews);
+        }
+    }
+
+    /**
+     * Updates the colors of the clock to a state representing "now".
+     * @param calendar 
+     * 
+     * @param context used to grab hold of some xml resource values such as
+     *        default background color.
+     */
+    public void updateView(RemoteViews remoteViews, Calendar calendar) {
+
+        if (mDefaultDigitBackgrundColor == -1) {
+            mDefaultDigitBackgrundColor = getResources().getColor(
+                    R.color.default_digit_background_color);
+        }
+
+        int hoursX0 = calendar.get(Calendar.HOUR_OF_DAY) / 10;
+        int hours0X = calendar.get(Calendar.HOUR_OF_DAY) % 10;
+        int minutesX0 = calendar.get(Calendar.MINUTE) / 10;
+        int minutes0X = calendar.get(Calendar.MINUTE) % 10;
+        int secondsX0 = calendar.get(Calendar.SECOND) / 10;
+        int seconds0X = calendar.get(Calendar.SECOND) % 10;
+
+        for (int i = 0; i <= 9; i++) {
+            mDigitsColor[i] = 0;
+        }
+
+        mDigitsColor[hoursX0] = setOrBlendDigitColorWithColor(
+                mDigitsColor[hoursX0], mPrimaryHourColor);
+        mDigitsColor[hours0X] = setOrBlendDigitColorWithColor(
+                mDigitsColor[hours0X], mSecondaryHourColor);
+        mDigitsColor[minutesX0] = setOrBlendDigitColorWithColor(
+                mDigitsColor[minutesX0], mPrimaryMinuteColor);
+        mDigitsColor[minutes0X] = setOrBlendDigitColorWithColor(
+                mDigitsColor[minutes0X], mSecondaryMinuteColor);
+        mDigitsColor[secondsX0] = setOrBlendDigitColorWithColor(
+                mDigitsColor[secondsX0], mPrimarySecondColor);
+        mDigitsColor[seconds0X] = setOrBlendDigitColorWithColor(
+                mDigitsColor[seconds0X], mSecondarySecondColor);
+
+        for (int i = 0; i <= 9; i++) {
+            if (mDigitsColor[i] == 0) {
+                mDigitsColor[i] = mDefaultDigitBackgrundColor;
+            }
+        }
+
+        // Set the colors to the views.
+        for (int i = 0; i <= 9; i++) {
+            //
+            remoteViews.setInt(DIGIT_VIEWS_INDEX[i], "setBackgroundColor",
+                    mDigitsColor[i]);
+        }
+    }
+
+    /**
+     * Blends the two colors unless the first color is pitch black with 0 alpha,
+     * for that corner-case it will return the second color
+     * 
+     * @param c1 first color.
+     * @param c2 second color.
+     * @return a blend of the two color, unless above stated condition applies.
+     */
+    private int setOrBlendDigitColorWithColor(int c1, int c2) {
+        if (c1 != 0) {
+            return ColorUtil.screenBlendTwoColors(c1, c2);
+        } else {
+            return c2;
+        }
+    }
+
+}
